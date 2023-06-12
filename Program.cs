fa76c0bade8ae752609a0c98650bb2ba6ffbebef
GeorgyShefer <132808044+GeorgyShefer@users.noreply.github.com>
2023-06-12 16:11:16 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+using ConsoleApp1;
+class DirectoryExtension
+{
+    public static long dirSize(DirectoryInfo d)
+    {
+        long size = 0;
+
+        var f = d.GetFiles();
+
+        foreach (var fi in f)
+        {
+            size += fi.Length;
+        }
+
+        DirectoryInfo[] dis = d.GetDirectories();
+
+        foreach (DirectoryInfo di in dis)
+        {
+            size += dirSize(d);
+        }
+        return size;
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+
+        DriveInfo[] driveC = DriveInfo.GetDrives();
+        foreach (DriveInfo c in driveC.Where(c => c.DriveType == DriveType.Fixed))
+        {
+            DirectoryInfo dirC = c.RootDirectory;
+
+            WriteFileInfo(dirC);
+        }
+
+    }
+
+    public static void WriteFileInfo(DirectoryInfo rootFolders)
+    {
+        Console.WriteLine("Файлы");
+        Console.WriteLine();
+
+        foreach (var folder in rootFolders.GetFiles())
+        {
+            Console.WriteLine(folder.Name + " " + folder.Length + " байт");
+        }
+    }
+
+}
+
